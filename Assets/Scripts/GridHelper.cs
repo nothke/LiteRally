1d4aa52d6129f9e73fc0ac37966ebe93e719d9f3
Ivan Notaros <nothke@gmail.com>
2016-12-25 17:19:58 +0100

    Made GridHelper to help with placing grids by showing lines

---
+++ Assets/Scripts/GridHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridHelper : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Vector3 fl = transform.position + transform.forward * 2 - transform.right * 1;
+        Vector3 fr = transform.position + transform.forward * 2 + transform.right * 1;
+        Vector3 bl = transform.position - transform.forward * 2 - transform.right * 1;
+        Vector3 br = transform.position - transform.forward * 2 + transform.right * 1;
+
+        Gizmos.DrawLine(fl, fr);
+        //Gizmos.DrawLine(bl, br);
+        Gizmos.DrawLine(fl, bl);
+        Gizmos.DrawLine(fr, br);
+    }
+}
