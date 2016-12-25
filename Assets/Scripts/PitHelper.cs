862c549831e4951ae0bf4c32194939bf6bcbc67c
Ivan Notaros <nothke@gmail.com>
2016-12-25 17:41:18 +0100

    Made PitHelper, added a few pits (not working ATM), added lines to the test track texture

---
+++ Assets/Scripts/PitHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitHelper : MonoBehaviour {
+
+    private void OnDrawGizmos()
+    {
+        Vector3 fl = transform.position + transform.forward * 2.5f - transform.right * 2f;
+        Vector3 fr = transform.position + transform.forward * 2.5f + transform.right * 2f;
+        Vector3 bl = transform.position - transform.forward * 2.5f - transform.right * 2f;
+        Vector3 br = transform.position - transform.forward * 2.5f + transform.right * 2f;
+
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawLine(fl, fr);
+        Gizmos.DrawLine(bl, br);
+        Gizmos.DrawLine(fl, bl);
+        Gizmos.DrawLine(fr, br);
+    }
+}
