5a54153cb23b1630629ba11afada507fd6a34423
Ivan Notaros <nothke@gmail.com>
2016-12-29 14:56:07 +0100

    Cleanup

--- Assets/Scripts/TrackManager.cs
+++ Assets/Scripts/TrackManager.cs
@@ -335,7 +335,6 @@ public class TrackManager : MonoBehaviour
         if (!GO)
         {
             GO = new GameObject(System.IO.Path.GetFileName(hierarchyPath));
-            Debug.Log(GO.name);
             GO.transform.parent = atParent;
         }
 
