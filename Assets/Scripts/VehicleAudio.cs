81ee337383101f3d7e8f4709b6aca95d7e928a1f
Ivan Notaros <nothke@gmail.com>
2017-01-20 04:30:08 +0100

    First audio script with engine

---
+++ Assets/Scripts/VehicleAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleAudio : MonoBehaviour
+{
+
+    VehicleController control;
+
+    public AudioSource engineSource;
+    public float engineMinPitch = 0.2f;
+    public float engineMaxPitch = 1;
+
+    public AudioSource tireSquealSource;
+
+    void Start()
+    {
+        control = GetComponent<VehicleController>();
+    }
+
+    float refVelo;
+    float throttle;
+
+    void Update()
+    {
+        if (engineSource)
+        {
+            engineSource.volume = 0.1f;
+
+            throttle = Mathf.SmoothDamp(throttle, control.accelInput, ref refVelo, 0.2f);
+
+            float pitchT = Mathf.Lerp(throttle, control.speed * 0.02f, 0.5f);
+
+            engineSource.pitch = Mathf.Lerp(engineMinPitch, engineMaxPitch, pitchT);
+        }
+    }
+}
