dacbabdb146e6fbc9402149c99e49fc3656c9816
Ivan Notaros <nothke@gmail.com>
2017-07-06 15:22:17 +0200

    Made sure tester is destroyed in build. Updated scene

--- Assets/Scripts/TrackTester.cs
+++ Assets/Scripts/TrackTester.cs
@@ -10,13 +10,20 @@ public class TrackTester : MonoBehaviour
     public string trackName;
     public string layoutName;
 
+    void Awake()
+    {
+#if !UNITY_EDITOR
+		Destroy(this);
+#endif
+    }
+
     // Just to make it enablable in editor:
     void Start() { }
 
     public void SetNamesToTrackManager()
     {
         if (loadTrackInTrackManager)
-			TrackManager.e.InitThisTrack();
+            TrackManager.e.InitThisTrack();
         else
             TrackManager.e.DeserializeTrack(trackName, layoutName);
     }
